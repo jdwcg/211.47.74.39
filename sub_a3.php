873c2841fff5ec63e8c74433d9cd2a87ea2f3d63
jdwcg <73451502+jdwcg@users.noreply.github.com>
2021-12-03 17:30:48 +0900

    2

--- sub_a3.php
+++ sub_a3.php
@@ -44,12 +44,13 @@
                                   연구 기간은 2021년 7월 1일부터 2026년 6월 30일까지 총 5년이며 과제를 수행하기 위한 지원 사업비는 매년 10억이다.
                               </p>
                               <p class="second">
-                                <b>연구 주제와 부제</b><br>
-                                &lt;K종교의 이미지, 참여 그리고 실천, K-Religion: The Image, Participation and Practice&gt;
-                                K종교의 독자적 이미지 구축을 위한 멀티미디어 강의 및 다큐멘터리 영상 제작 플랫폼
-                                Making the Multi-media Lecture and Documentary Film Platform for the Integral Image of K-Religion
+                                <h3 style="padding: 2rem 0 3rem; text-align: center;">연구 주제와 부제</h3>
+                                K종교의 이미지, 참여 그리고 실천, <br/>
+                                <!-- K-Religion: The Image, Participation and Practice <br/> -->
+                                K종교의 독자적 이미지 구축을 위한 멀티미디어 강의 및 다큐멘터리 영상 제작 플랫폼<br/>
+                                <!-- Making the Multi-media Lecture and Documentary Film Platform for the Integral Image of K-Religion -->
                               </p>
-                              <p class="third">
+                              <p class="third"><br/>
                                 연구소 플랫폼에서는 5년간 50개(총 600시간, 3,600개 비디오 클립)의 동영상 강좌를 제작하여 K-Mooc 및 유튜브에 업로딩한다.
                                 강의는 한국어, 영어 그리고 스페인어를 지원한다. K-종교를 주제로 두 편의 다큐멘터리를 출품하며,
                                 3번의 국제회의, 소스북, 연구서, 백서를 간행하고 보조 방송을 제작하여 동영상을 수강하는 세계 각국 학생들의 강좌 이해를 돕는다.
@@ -64,8 +65,21 @@
         </div>
     </section>
     <style media="screen">
+    .project {overflow: hidden;}
       .project > p {
         text-align: justify;
+        position: relative;
+      }
+      .first { word-break: break-all; }
+      @media(min-width: 640px) {
+        .first {
+          position: absolute;
+          width: 480px;
+          left: 50%; transform: translate(-50%);
+          padding: 1rem 0;
+          border-top: 1px solid #ccc;
+          border-bottom: 1px solid #ccc;
+        }
       }
     </style>
 
